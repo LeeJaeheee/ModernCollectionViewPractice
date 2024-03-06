b3ca2998dd99f0a975723c3341ca76aa9f660be7
LeeJaeheee <sns3434@gmail.com>
2024-03-07 00:30:19 +0900

    Initial Commit

---
+++ ModernCollectionViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCollectionViewPractice
+//
+//  Created by 이재희 on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
